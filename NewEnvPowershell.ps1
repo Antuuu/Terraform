259c158115d3995240062f2e5c90df6bb7a95eec
Antuuu <53739270+Antuuu@users.noreply.github.com>
2020-02-05 15:29:50 +0100

    Create NewEnvPowershell.ps1
    Script used to create multiple new environments using Powershell to replace the environment name.

---
+++ NewEnvPowershell.ps1
@@ -0,0 +1,29 @@
+Param(
+    [string]$EnvName,
+    [string]$State 
+    )
+if ($State -eq 'create') {
+    if (Test-Path .\$EnvName){
+        cd .\$EnvName
+        terraform init
+        terraform apply -auto-approve
+        terraform output -json >> output.json
+    }
+    else{
+        Copy-Item -Recurse .\Cloud_DevOps\ .\$EnvName
+        (Get-Content .\$EnvName\global_variables.tf) | ForEach-Object {
+        $_.replace('#EnvironmentName#', $EnvName) #.replace('somethingElse1', 'somethingElse2')
+        } | Set-Content .\$EnvName\global_variables.tf
+        cd .\$EnvName
+        terraform init
+        terraform apply -auto-approve
+        terraform output -json >> output.json
+    }
+}
+elseif ($State -eq 'destroy') {
+    cd .\$EnvName
+    terraform init
+    terraform destroy -auto-approve
+    cd ..\.
+    Remove-Item .\$EnvName -Recurse -Force
+}
